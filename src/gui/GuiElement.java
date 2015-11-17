5da559c20319c4a882716d6ca86118238d1e0b09
Gollinator <gplebanek@web.de>
2015-11-17 14:37:11 +0100

    DrawBorder a GuiElement Method

--- src/gui/GuiElement.java
+++ src/gui/GuiElement.java
@@ -2,39 +2,40 @@ package gui;
 
 import gameEngine.Screen;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public abstract class GuiElement implements Comparable<GuiElement> {
 
-	private ArrayList<ActionListener> actionlistener = new ArrayList<ActionListener>(
-			1);
+	private ArrayList<ActionListener> actionlistener = new ArrayList<ActionListener>(1);
 	private int layer;
 	private int x, y;
-	private int width,height;
+	private int width, height;
+	private boolean border = false;
 
-	public Rectangle getBounds(){
+	public Rectangle getBounds() {
 		return new Rectangle(getX(), getY(), getWidth(), getHeight());
 	}
-	
-	public void setWidth(int width){
+
+	public void setWidth(int width) {
 		this.width = width;
 	}
 
 	public int getWidth() {
 		return width;
 	}
-	
-	public void setHeight(int height){
+
+	public void setHeight(int height) {
 		this.height = height;
 	}
-	public int getHeight(){
+
+	public int getHeight() {
 		return height;
 	}
-	
+
 	public void setLayer(int layer) {
 		this.layer = layer;
 	}
@@ -70,7 +71,7 @@ public abstract class GuiElement implements Comparable<GuiElement> {
 	public abstract void onDraw(Graphics2D g2d);
 
 	public abstract void onUpdate(Screen screen);
-	
+
 	public int compareTo(GuiElement arg0) {
 		if (this.layer > arg0.layer) {
 			return 1;
@@ -79,5 +80,19 @@ public abstract class GuiElement implements Comparable<GuiElement> {
 		} else {
 			return 0;
 		}
+	}
+
+	public void drawBorder(Graphics2D g2d) {
+		if (border) {
+			g2d.setColor(Color.BLACK);
+			g2d.drawLine(getX(), getY(), getX() + getWidth(), getY());
+			g2d.drawLine(getX(), getY(), getX(), getY() + getHeight());
+			g2d.drawLine(getX() + getWidth(), getY(), getX() + getWidth(), getY() + getHeight());
+			g2d.drawLine(getX(), getY() + getHeight(), getX() + getWidth(), getY() + getHeight());
+		}
+	}
+
+	public void setBorder(boolean border) {
+		this.border = border;
 	}
 }
